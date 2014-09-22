1c0b865422b0f20e605dcc76bf9f33036cab5170
Louis Acresti <louis.acresti@gmail.com>
2014-09-21 21:22:26 -0400

    difflet for equality errors; some cleanup

--- test/fail.js
+++ test/fail.js
@@ -13,3 +13,13 @@ test('2 === 1', function(assert) {
   assert.plan(1);
   assert.equal(2, 1);
 });
+
+test('{a: 404} === {a: 42}', function(assert) {
+  assert.plan(1);
+  assert.deepEqual({a: 404}, {a: 42});
+});
+
+test('"Hell, world!" === "Hello, world!"', function(assert) {
+  assert.plan(1);
+  assert.equal("Hell, world!", "Hello, world!");
+})
